9f33038551839d1352d62d6b33ece45d5a53767c
Wing Chin <wingchin@lifeguard.nl>
2020-04-04 20:56:44 +0200

    upgrade to bootstrap 4

--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -1,6 +1,3 @@
-//= require bootstrap/transition
-//= require bootstrap/tab
-
 /*
 ==============================
 
@@ -26,17 +23,17 @@ $(document).ready(function() {
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
   if ($(".navbar").offset().top > 50) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
+    $(".fixed-top").addClass("top-nav-collapse");
   } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    $(".fixed-top").removeClass("top-nav-collapse");
   }
 });
 
-$('.navbar-toggle:visible').click(function() {
+$('.navbar-toggler:visible').click(function() {
   if($('.navbar-collapse:visible').length) {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    $(".fixed-top").removeClass("top-nav-collapse");
   } else {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
+    $(".fixed-top").addClass("top-nav-collapse");
   }
 });
 
@@ -50,9 +47,9 @@ $('.navbar-toggle:visible').click(function() {
 */
 $(window).scroll(function() {
   if ($(this).scrollTop() > 100) {
-      $('#back-to-top').fadeIn();
+    $('#back-to-top').fadeIn();
   } else {
-      $('#back-to-top').fadeOut();
+    $('#back-to-top').fadeOut();
   }
 });
 
@@ -86,79 +83,3 @@ $(function() {
     }
   });
 });
-
-
-/*
-==============================
-
-  TABS
-
-==============================
-*/
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show');
-});
-
-
-/*
-==============================
-
-  GOOGLE MAPS
-  http://www.labnol.org/internet/embed-google-maps-background/28457/
-
-==============================
-*/
-if ($('#map').length) {
-  function showGoogleMaps() {
-    var latLng = new google.maps.LatLng(position[0], position[1]);
-
-    // Basic options for a simple Google Map
-    var mapOptions = {
-      zoom: 15,
-      center: latLng,
-
-      // Disables the default Google Maps UI components
-      disableDefaultUI: true,
-
-      // Styles from Snazzy Maps
-      // https://snazzymaps.com/style/19/subtle
-      styles: [
-        {
-          "stylers": [{ "saturation": -70 }, { "lightness": 37 }, { "gamma": 1.15 }]
-        },
-        {
-          "featureType": "poi",
-          "stylers": [{ "visibility": "off" }]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "geometry",
-          "stylers": [{ "lightness": 10 }]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [{ "lightness": 50 }, { "saturation": 20 }, { "hue": "#ffffff" }]
-        }
-      ]
-    };
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    // Show the marker at the location
-    var pinImage = new google.maps.MarkerImage(
-      "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|cccccc"
-    );
-
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-      icon: pinImage,
-    });
-  }
-
-  // When the window has finished loading create our google map below
-  google.maps.event.addDomListener(window, 'load', showGoogleMaps);
-}
